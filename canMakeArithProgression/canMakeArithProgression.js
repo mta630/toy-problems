215611d0446beddd153f021f5bfc627cd7406a64
mta630 <Michael.t.albert89@gmail.com>
2020-07-08 12:57:53 -0500

    Completed canMakeArithProgression

--- canMakeArithProgression/canMakeArithProgression.js
+++ canMakeArithProgression/canMakeArithProgression.js
@@ -3,5 +3,20 @@
  * @return {boolean}
  */
 var canMakeArithmeticProgression = function(arr) {
-
+    var asc = arr.slice().sort((a, b) => a - b);
+    
+    var ascInc = asc[1] - asc[0];
+    var result = true;
+    
+    for (var i = 0; i < asc.length; i ++) {
+        if (asc[i+1]) {
+            if (asc[i+1] - asc[i] === ascInc) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
 };
