5b0ea537144655ee3272a6d2e164aede290a3296
mta630 <Michael.t.albert89@gmail.com>
2020-07-08 13:30:57 -0500

    Completed oddCells

--- cellsWithOddValueMatrix/cellsWithOddValueMatrix.js
+++ cellsWithOddValueMatrix/cellsWithOddValueMatrix.js
@@ -5,5 +5,32 @@
  * @return {number}
  */
 var oddCells = function(n, m, indices) {
+    var result = [];    
     
+    for (var i = 0; i < n; i++) {
+        var arr = new Array(m).fill(0);
+        result.push(arr);
+    }
+    
+    indices.forEach((arr) => {
+        result[arr[0]].forEach((x, j) => {
+            result[arr[0]][j] ++;
+        })
+        result.forEach((x, i) => {
+            result[i][arr[1]] ++;
+        })
+    })
+    
+    var realResult = 0;
+    
+    result.forEach((y) => {
+        y.forEach((z) => {
+            if (z % 2 === 1) {
+                realResult ++;
+            }
+        })
+    })
+    
+    return realResult;
+     
 };
