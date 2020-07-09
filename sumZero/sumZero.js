575dee0d35d0360111842fde530094debbddd0a2
mta630 <Michael.t.albert89@gmail.com>
2020-07-09 12:18:51 -0500

    sumZero Complete

--- sumZero/sumZero.js
+++ sumZero/sumZero.js
@@ -3,5 +3,26 @@
  * @return {number[]}
  */
 var sumZero = function(n) {
+    var result = new Array(n).fill(0);
     
+    if (n % 2 != 0) {
+        var counter = -1 * (n - 2);
+        for (var i = 0; i < n; i ++) {
+            if (i !== Math.floor(n / 2)) {
+                result[i] = counter;
+                counter += 2;   
+            }
+        }
+    } else {
+        var counter = -1 * (n)
+        for (var i = 0; i < n; i ++) {          
+            if (i === (n / 2)) {
+                counter += 2
+            }
+            result[i] = counter;
+            counter +=2;
+        }
+    }
+    
+    return result;
 };
