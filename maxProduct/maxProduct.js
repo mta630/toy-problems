272e415fc39de454f2c0caf5bec9fadfbb0a882e
mta630 <Michael.t.albert89@gmail.com>
2020-07-09 11:55:58 -0500

    maxProduct Complete

--- maxProduct/maxProduct.js
+++ maxProduct/maxProduct.js
@@ -3,5 +3,7 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    
+    var sorted = nums.sort((a, b) => a - b);
+    var len = sorted.length;
+    return (sorted[len - 1] - 1) * (sorted[len - 2] - 1);
 };
