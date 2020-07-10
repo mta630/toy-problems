4d4a8fc51446378fc4899fe7576fc6e1aff528d6
mta630 <michael.t.albert89@gmail.com>
2020-07-10 10:39:20 -0500

    Completed arrayMaxConsecutiveSum

--- arrayConsecutiveSum/arrayConsecutiveSum.js
+++ arrayConsecutiveSum/arrayConsecutiveSum.js
@@ -1,3 +1,21 @@
 function arrayMaxConsecutiveSum(inputArray, k) {
+  var results = [];
 
+  for (var i = 0; i < inputArray.length; i ++) {
+      var num = inputArray[i];
+
+      for (var j = i + 1; j < (k + i); j ++) {
+          if (inputArray[j]) {
+              num += inputArray[j]
+          } else {
+              break;
+          }
+      }
+      if (!isNaN(num)) {
+          results.push(num);
+      }
+  }
+
+  return Math.max(...results);
 }
+
