158cc9b62ae160b4e9977b72bf89b49d0f323104
mta630 <michael.t.albert89@gmail.com>
2020-07-10 10:26:36 -0500

    Finished freqAlphabets

--- freqAlphabets/freqAlphabets.js
+++ freqAlphabets/freqAlphabets.js
@@ -3,5 +3,14 @@
  * @return {string}
  */
 var freqAlphabets = function(s) {
-
+  let res = '';
+  for(let i=s.length-1;i>-1;i--){
+      if(s[i] === '#'){
+          res=toAlpha(s[i-2]+s[i-1])+res;
+          i-=2;
+      }else res=toAlpha(s[i])+res;
+  }
+  return res
 };
+
+var toAlpha = (s)=>String.fromCharCode(parseInt(s)+96);
